d088ed5d8c1afda0e8163782281b262831573fd7
Daniyal1303 <daniyalahmad@devminified.com>
2026-01-23 07:57:43 +0500

    prisma database connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
+import { Pool } from 'pg';
   
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   
   constructor() {
-    super({  log: ['query', 'info', 'warn', 'error'] });
+
+    const pool = new Pool({
+      connectionString:process.env.DATABASE_URL
+    })
+    const adapter = new PrismaPg(pool)
+    super({adapter,  log: ['query', 'info', 'warn', 'error'] });
   }
 
   async onModuleInit() {
